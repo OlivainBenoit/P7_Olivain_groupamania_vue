dd0e013988a6694baa05b246861f3b4862c24545
OlivainBenoit <olivain.benoit@gmail.com>
2022-06-23 16:46:59 +0200

    async back login/signin

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -3,52 +3,41 @@ const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
 
-exports.signup = (request, response) => {
-  bcrypt
-    .hash(request.body.password, 10)
-    .then((hash) => {
-      const user = new User({
-        email: request.body.email,
-        password: hash,
-        username: request.body.username,
-      });
-      user
-        .save()
-        .then(() =>
-          response.status(201).json({ message: "Utilisateur crée !" })
-        )
-        .catch((error) => response.status(400).json({ error }));
-    })
-    .catch((error) => response.status(500).json({ error }));
+exports.signup = async (request, response) => {
+  const hash = await bcrypt.hash(request.body.password, 10);
+  try {
+    const user = new User({
+      email: request.body.email,
+      password: hash,
+      username: request.body.username,
+    });
+    await user.save();
+
+    response.status(201).json({ message: "Utilisateur crée !" });
+  } catch (e) {
+    response.status(500).json({ e });
+  }
 };
 
-exports.login = (request, response) => {
-  console.log("request :", request.body);
-  User.findOne({ email: request.body.email })
-    .then((user) => {
-      console.log(user);
-      if (!user) {
-        return response.status(401).json({ error: "Utilisateur non trouvé" });
-      }
-      bcrypt
-        .compare(request.body.password, user.password)
-        .then((valid) => {
-          console.log("valid :", valid);
-          if (!valid) {
-            return response
-              .status(401)
-              .json({ error: "Mot de passe incorrect" });
-          }
-          response.status(200).json({
-            userId: user._id,
-            token: jwt.sign({ userId: user._id }, "RANDOM_TOKEN_SECRET", {
-              expiresIn: "24h",
-            }),
-            isAdmin: user.isAdmin,
-            username: user.username,
-          });
-        })
-        .catch((error) => response.status(500).json({ error }));
-    })
-    .catch((error) => response.status(500).json({ error }));
+exports.login = async (request, response) => {
+  const user = await User.findOne({ email: request.body.email });
+  try {
+    if (!user) {
+      return response.status(401).json({ error: "Utilisateur non trouvé" });
+    }
+    const valid = bcrypt.compare(request.body.password, user.password);
+    if (!valid) {
+      return response.status(401).json({ error: "Mot de passe incorrect" });
+    }
+    response.status(200).json({
+      userId: user._id,
+      token: jwt.sign({ userId: user._id }, "RANDOM_TOKEN_SECRET", {
+        expiresIn: "24h",
+      }),
+      isAdmin: user.isAdmin,
+      username: user.username,
+    });
+  } catch (e) {
+    response.status(500).json({ error });
+  }
 };
